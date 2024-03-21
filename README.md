875c0ae8b2fc15274953a76aa778db67bddd7d17
PencilShavings <eb.pencilshavings@protonmail.com>
2024-03-21 13:33:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Sparrow Wallet AppImage
+A script that will build an AppImage of [Sparrow Wallet](https://github.com/sparrowwallet/sparrow).
+
+It will temporarily download and run `appimagetool` against the set version of Sparrow,
+building an AppImage that can be found in `~/Documents`.
+
+### Why an AppImage? Sparrow is pretty much a a standalone binary.
+Why? Because I wanted to? I prefer it over running it out of an extracted tar file.
+
+AppImages provides the ability to set custom `.config` and `$HOME` directories.
+This allows for isolated settings/configs/caches to exist in their own place without
+cluttering up you main `$HOME`.
+
+But Sparrow has the `-d` option that can specify a custom directory!
+
+Well that means one has to launch Sparrow from the cli or create a custom launcher with those arguments. Might as well just package it up with the arguments made.
+
+### NOTES
+- Sparrow.AppImage does NOT respect Sparrow.AppImage.config as it does not use the _.config_ directory.
+- By default Sparrow seems to be hard coded to use _/home/$USER/.sparrow_ and not _$HOME/.sparrow_, 
+I suspect, I haven't checked the actual code. It just came up in testing.
+  In order to respect _$HOME_, `-d $HOME/.sparrow` is passed to Sparrow in the AppRun.
+  This allows for _/home/$USER/.sparrow_ & _Sparrow.AppImage.home/.sparrow_ to be used.
+  If you want to store _Sparrow.AppImage.home/.sparrow_ somewhere else just symlink the directory.
---
+++ sparrow-appimage-build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+WORKDIR="/tmp/sparrow-wallet-appimage-${RANDOM}"
+mkdir -v ${WORKDIR}
+cd ${WORKDIR}
+
+VERSION='1.8.4'
+APPDIR='Sparrow.AppDir'
+TARFILE="sparrow-${VERSION}-x86_64.tar.gz"
+
+# Download appimagetool
+curl -L -o appimagetool "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool
+
+# Setup AppDir
+mkdir -v ${APPDIR}
+cd ${APPDIR}
+
+# Download Sparrow
+curl -L -O "https://github.com/sparrowwallet/sparrow/releases/download/${VERSION}/${TARFILE}"
+
+# Validate Download
+# curl -L -O "https://github.com/sparrowwallet/sparrow/releases/download/${VERSION}/sparrow-${VERSION}-manifest.txt.asc"
+curl -L -O "https://github.com/sparrowwallet/sparrow/releases/download/${VERSION}/sparrow-${VERSION}-manifest.txt"
+sha256sum --check sparrow-1.8.4-manifest.txt --ignore-missing
+
+if [ "$?" = "0" ]; then
+    echo "Checksum PASSED"
+    rm sparrow-${VERSION}-manifest.txt
+else
+    echo "Checksum FAILED, Exiting..."
+    cd
+    rm -rf ${WORKDIR}
+    exit 1
+fi
+
+# Extract & remove un-needed files
+tar -xzf ${TARFILE} --strip 1 -C .
+rm -v ${TARFILE}
+
+# Link Icon
+ln -sr ./lib/Sparrow.png Sparrow.png
+
+# Generate AppRun
+echo -e '#!/bin/bash
+cd "$(dirname "$0")"
+./bin/Sparrow -d $HOME/.sparrow' > AppRun
+chmod +x AppRun
+
+# Generate .desktop launcher
+echo -e '[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Sparrow Wallet
+Icon=Sparrow
+Exec=AppRun
+Comment=Financial self soverignty
+Categories=Finance;Office;
+Terminal=false' > Sparrow.desktop
+
+# Build AppImage
+cd ..
+ARCH=x86_64 ./appimagetool ./${APPDIR} Sparrow-${VERSION}.AppImage
+mv -v Sparrow-${VERSION}.AppImage ~/Documents
+
+cd
+rm -rf ${WORKDIR}
+
